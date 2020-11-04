c53de0a134e90f553057dcb9c20d8df91dd6fb5c
Juan_Sebastian2 <sebasjuanpenarreta2018@gmail.com>
2020-11-04 18:46:48 -0500

    Expresiones3

---
+++ Expresiones3/Expresiones3.1.java
@@ -0,0 +1,14 @@
+public class Expresiones3 {
+	public static void main(String[] args) {
+	float x = 2100;
+	float y = 1440;
+	float z = 456000;
+	float w = 3650;
+	float resultado;
+
+	resultado = x * y * z * w;
+
+	System.out.println("Dados x = 2100 ; y = 1440 ; z = 456000 ; w = 3650");
+	System.out.println("\nEl resultado de ( x * y * z * w ) es " + "resultado");
+	}
+}
---
+++ Expresiones3/Expresiones3.2.java
@@ -0,0 +1,13 @@
+public class Expresiones3 {
+	public static void main(String[] args) {
+	float x = 2;
+	float y = 3;
+	float z = 2;
+	float resultado;
+
+	resultado = x + y + z;
+
+	System.out.println("Dados x = 2 ; y = 3 ; z = 2");
+	System.out.println("\nEl resultado de ( x + y + z ) es " + "resultado");
+	}
+}
